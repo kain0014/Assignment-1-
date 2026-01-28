d009a5ad8615bd538640d39b3da71d6fff2f27cd
kain0014 <bishtkavya045@gmail.com>
2026-01-28 22:50:33 +0530

    initial Commit

---
+++ Assignment_1_prob_ 1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+Sum = num1 + num2
+print("Addition:", Sum)
+Difference = num1 - num2
+print("Subtraction:", Difference)
+Product = num1 * num2
+print("Multiplication:", Product)
+Divide = num1 / num2
+print("Division:", Divide)
+
+
